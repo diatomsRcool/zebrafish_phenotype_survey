c2db409fc152cd37841692bc95cf8305afe375ec
SkylarMarvel <swmarvel@ncsu.edu>
2021-07-30 12:30:07 +0700

    Added mean concordance

--- analysis/2_computation.r
+++ analysis/2_computation.r
@@ -6,7 +6,8 @@ for (annotation_type in c('general', 'granular')) {
 
   df <- read.csv(paste0('data/formatted_data_', annotation_type, '.csv'), stringsAsFactors = F)
   df <- filterData(df)
-
+  df <- addConcurrence(df)
+  
   # Factor categorical variables
   df$Survey     <- factor(df$Survey)
   df$Embryo     <- factor(df$Embryo)
@@ -17,13 +18,13 @@ for (annotation_type in c('general', 'granular')) {
   # Fleiss Kappa
   # Embryos can be in multiple categories, is that a problem for stats calc?
   # ------------------------------------------------------------
-  
+
   for (survey in 1:2) {
     tbl   <- fleissTable(df[df$Survey == survey, ])
     kappa <- fleissKappa(tbl)
     fleissSave(tbl, kappa, paste0('output/', annotation_type, '_fleissKappa_survey', survey, '.csv'))
   }
-  
+
   # ------------------------------------------------------------
   # Intraclass correlation
   # ------------------------------------------------------------
@@ -51,11 +52,50 @@ for (annotation_type in c('general', 'granular')) {
   # ------------------------------------------------------------
   # Fit glm
   # ------------------------------------------------------------
-  
-  df <- addConcurrence(df)
+
   fit <- glm(Concur ~ Survey + Embryo + Annotation + Rater, data = df, family = 'binomial')
   sink(paste0('output/', annotation_type, '_glm.txt'))
   print(summary(fit))
   sink()
+
+  # ------------------------------------------------------------
+  # Rater mean concordance
+  # ------------------------------------------------------------
+  
+  raters <- levels(df$Rater)
+  raterMeanConcordance <- setNames(cbind(
+    raters,
+    data.frame(do.call(rbind, lapply(raters, function(x) {
+      c(mean(df$Concur[df$Rater == x]),
+        mean(df$Concur[df$Rater == x & df$Survey == 1]),
+        mean(df$Concur[df$Rater == x & df$Survey == 2]))
+    })))
+  ), c('Rater', 'All', 'Survey1', 'Survey2'))
+  raterMeanConcordance <- rbind(
+    raterMeanConcordance,
+    c('mean', mean(raterMeanConcordance$All), mean(raterMeanConcordance$Survey1), mean(raterMeanConcordance$Survey2)),
+    c('sd',     sd(raterMeanConcordance$All),   sd(raterMeanConcordance$Survey1),   sd(raterMeanConcordance$Survey2))
+  )
+  write.csv(raterMeanConcordance, paste0('output/', annotation_type, '_mean_concordance_rater.csv'), row.names = F)
+
+  # ------------------------------------------------------------
+  # Annotation mean concordance
+  # ------------------------------------------------------------
+  
+  annotations <- levels(df$Annotation)
+  annotMeanConcordance <- setNames(cbind(
+    annotations,
+    data.frame(do.call(rbind, lapply(annotations, function(x) {
+      c(mean(df$Concur[df$Annotation == x]),
+        mean(df$Concur[df$Annotation == x & df$Survey == 1]),
+        mean(df$Concur[df$Annotation == x & df$Survey == 2]))
+    })))
+  ), c('Annotation', 'All', 'Survey1', 'Survey2'))
+  annotMeanConcordance <- rbind(
+    annotMeanConcordance,
+    c('mean', mean(annotMeanConcordance$All), mean(annotMeanConcordance$Survey1), mean(annotMeanConcordance$Survey2)),
+    c('sd',     sd(annotMeanConcordance$All),   sd(annotMeanConcordance$Survey1),   sd(annotMeanConcordance$Survey2))
+  )
+  write.csv(annotMeanConcordance, paste0('output/', annotation_type, '_mean_concordance_annotation.csv'), row.names = F)
   
 }
